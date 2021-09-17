e5955cf427b38b554ae6186dbf857663cd74ee54
Konstantin <87612418+ShootkaXd@users.noreply.github.com>
2021-09-17 17:06:53 +0300

    Add files via upload

---
+++ 2/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+typedef char TEXT;
+
+void kl_slov()
+{
+TEXT s[20];
+int slov, count = 0, i;
+printf("Введите предложение: ");
+gets(s);
+
+for(i=0; i<strlen(s)-1; i++)
+    if((s[i]==' ') && (s[i+1]!=' '))
+    count++;
+if(s[0]!=' ')
+    count++;
+
+printf("В предложение: %d слов", count);
+}
+
+
+
+
+
+
+
+
+
+int main()
+{
+setlocale(LC_ALL, "Rus");
+
+kl_slov();
+
+
+
+
+
+    return 0;
+}
---
+++ 3/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+enum week
+{
+Monday,
+
+Tuesday,
+
+Wednesday,
+
+Thursday,
+
+Friday,
+
+Saturday,
+
+Sunday
+
+};
+
+int main()
+{
+setlocale(LC_ALL, "Rus");
+int day;
+
+printf("¬ведите номер дн€ из недели: ");
+scanf("%d", &day);
+
+switch(day)
+{
+
+
+        case(1): printf("Monday"); break;
+
+        case(2): printf("Tuesday"); break;
+
+        case(3): printf("Wednesday"); break;
+
+        case(4): printf("Thursday"); break;
+
+        case(5): printf("Friday"); break;
+
+        case(6): printf("Saturday"); break;
+
+        case(7): printf("Sunday"); break;
+
+        default: printf("ERROR");
+        }
+
+
+
+
+    return 0;
+}
